574b781f8019fefb64370d4fb30e1707cdd64535
Prachi Agrawal <147470645+Prachi194agrawal@users.noreply.github.com>
2025-02-09 09:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ if __name__ == '__main__':
     import warnings
     warnings.filterwarnings('ignore', category=UserWarning)
     
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
